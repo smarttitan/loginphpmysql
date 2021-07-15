5a9f6701697d3f924a7f8429be899dd3c6132f2d
amirhossinhaghy <amirhossihaghy@gmail.com>
2021-07-16 01:40:16 +0430

    Server

--- app/src/main/java/com/example/loginphpmysql/MainActivity.java
+++ app/src/main/java/com/example/loginphpmysql/MainActivity.java
@@ -6,13 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 
